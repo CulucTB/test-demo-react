a5b6e221fe36d3d2505f9f836be828f388248c91
CulucTB <nguyenvanluc21051999@gmail.com>
2023-05-27 07:08:28 +0900

    video23

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -2,15 +2,22 @@
 // function component
 
 import React from 'react';
+import DisplayInfor from './Display';
 import UserInfor from './Userinfor';
 
 class MyComponent extends React.Component {
 
 
     render() {
+        const myInfor = ['ab', 'c', 'd']
         return (
             <div>
                 <UserInfor />
+                <br></br>
+                <DisplayInfor name="Hoi Dan It" age="30" />
+                <hr />
+                <DisplayInfor name="Eric" age="26" />
+
             </div>
         );
     }
