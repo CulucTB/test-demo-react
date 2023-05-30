6f779dc0aa99f72ca45eec06f9ec185217607a81
CulucTB <nguyenvanluc21051999@gmail.com>
2023-05-31 07:12:14 +0900

    video37

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,60 +1,109 @@
 // class companent
 // function component
 
-import React from 'react';
+import React, { useState } from 'react';
 import DisplayInfor from './Display';
 import AddUserinfor from './AddUserinfor';
 
-class MyComponent extends React.Component {
-    state = {
-        listUsers: [
+// class MyComponent extends React.Component {
+//     state = {
+//         listUsers: [
+//             { id: 1, name: "Hoi Dan It", age: "16" },
+//             { id: 2, name: "Eric", age: "26" },
+//             { id: 3, name: "Culuc", age: "69" },
+//         ]
+//     }
+// handleAddNewUser = (userObj) => {
+//     console.log(">>> check data from parent: ", userObj)
+//     this.setState({
+//         listUsers: [userObj, ...this.state.listUsers]
+//         // dùng cái này sẽ đẩy lên trên đầu 
+//         // listUsers: [...this.state.listUsers, userObj]
+//         // dùng cái này sẽ đẩy xuống cuối cùng
+//     })
+// }
+
+// handleDeleteUser = (userId) => {
+//     let listUsersClone = this.state.listUsers;
+//     listUsersClone = listUsersClone.filter(item => item.id !== userId);
+//     this.setState({
+//         listUsers: listUsersClone
+//     })
+// }
+
+// //JSX
+// render() {
+//     // DRY:don't reapt youself
+//     return (
+//         <>
+//             <br />
+//             <div className='a'>
+//                 <AddUserinfor
+//                     handleAddNewUser={this.handleAddNewUser}
+//                 />
+//                 <br></br>
+//                 <DisplayInfor
+//                     listUsers={this.state.listUsers}
+//                     handleDeleteUser={this.handleDeleteUser}
+
+//                 />
+//             </div>
+//             <div className='b'>
+
+//             </div>
+//         </>
+//     );
+// }
+
+// }
+
+const MyComponent = (props) => {
+    const [listUsers, setListUsers] = useState(
+        [
             { id: 1, name: "Hoi Dan It", age: "16" },
             { id: 2, name: "Eric", age: "26" },
             { id: 3, name: "Culuc", age: "69" },
         ]
-    }
-    handleAddNewUser = (userObj) => {
-        console.log(">>> check data from parent: ", userObj)
-        this.setState({
-            listUsers: [userObj, ...this.state.listUsers]
-            // dùng cái này sẽ đẩy lên trên đầu 
-            // listUsers: [...this.state.listUsers, userObj]
-            // dùng cái này sẽ đẩy xuống cuối cùng
-        })
+
+    )
+
+    const handleAddNewUser = (userObj) => {
+        setListUsers([userObj, ...listUsers])
+        // this.setState({
+        //     listUsers: [userObj, ...this.state.listUsers]
+        // listUsers: [...this.state.listUsers, userObj]
+        // dùng cái này sẽ đẩy xuống cuối cùng
+        // })
     }
 
-    handleDeleteUser = (userId) => {
-        let listUsersClone = this.state.listUsers;
+    const handleDeleteUser = (userId) => {
+        let listUsersClone = listUsers;
         listUsersClone = listUsersClone.filter(item => item.id !== userId);
-        this.setState({
-            listUsers: listUsersClone
-        })
+        setListUsers(listUsersClone);
+        //     this.setState({
+        //         listUsers: listUsersClone
+        //     })
     }
 
-    //JSX
-    render() {
-        // DRY:don't reapt youself
-        return (
-            <>
-                <br />
-                <div className='a'>
-                    <AddUserinfor
-                        handleAddNewUser={this.handleAddNewUser}
-                    />
-                    <br></br>
-                    <DisplayInfor
-                        listUsers={this.state.listUsers}
-                        handleDeleteUser={this.handleDeleteUser}
+    return (
+        <>
+            <br />
+            <div className='a'>
+                <AddUserinfor
+                    handleAddNewUser={handleAddNewUser}
+                />
+                <br></br>
+                <DisplayInfor
+                    listUsers={listUsers}
+                    handleDeleteUser={handleDeleteUser}
 
-                    />
-                </div>
-                <div className='b'>
+                />
+            </div>
+            <div className='b'>
 
-                </div>
-            </>
-        );
-    }
-
+            </div>
+        </>
+    )
 }
 
 export default MyComponent;
